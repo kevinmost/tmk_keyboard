ad5b765a04f0d82f7987d403bc1156ad786adb16
Kevin Most <kevin@kevinmost.com>
2016-02-09 12:32:44 -0500

    Added HHKB layout

--- keyboard/hhkb/keymap_kevin.c
+++ keyboard/hhkb/keymap_kevin.c
@@ -15,11 +15,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [0] =
-    KEYMAP(GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,BSPC, \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-           CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
+    KEYMAP(GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,BSPC, \
+           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
+           FN2, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0, \
-           LCTL,LGUI,LALT,          FN1,                RALT,RGUI,NO,RCTL),
+               LGUI,LALT,          FN1,                RALT,RGUI),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -35,15 +35,16 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */ 
     [1] = 
-    KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,TRNS,TRNS,   \
+    KEYMAP( ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,TRNS,TRNS,   \
            TRNS,TRNS,  UP,TRNS,TRNS,TRNS,TRNS,MPRV,MPLY,MNXT,TRNS,TRNS, TRNS, DEL,      \
            TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,VOLD,MUTE,VOLU,TRNS,TRNS,TRNS,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,            \
-           TRNS,TRNS,TRNS,          TRNS,               TRNS,TRNS,TRNS,TRNS),
+               TRNS,TRNS,          TRNS,               TRNS,TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_TOGGLE(1),
     [1] = ACTION_LAYER_TAP_KEY(1, KC_SPC),
+    [2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
 };
 
