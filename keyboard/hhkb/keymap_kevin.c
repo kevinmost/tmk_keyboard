157064af0b8cf9da09829542ecccc19a286e3422
Kevin Most <kevin@kevinmost.com>
2016-04-26 20:23:09 -0400

    Fixed up HHKB layout and added build.sh script

--- keyboard/hhkb/keymap_kevin.c
+++ keyboard/hhkb/keymap_kevin.c
@@ -1,45 +1,27 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Layer 0: Default Layer
-     * ,-----------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  `|BSp|
-     * |-----------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
-     * |-----------------------------------------------------------|
-     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
-     * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
-     * |-----------------------------------------------------------'
-     * |     |Gui|Alt  |         Space         |Alt  |Gui|   |     |
-     * `-----------------------------------------------------------'
+    /* 
+     * Layer 0: Default Layer
      */
-    [0] =
-    KEYMAP(GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,BSPC, \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
-           FN2, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0, \
-               LGUI,LALT,          FN1,                RALT,RGUI),
+    [0] = KEYMAP(
+        GRV ,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS,EQL,BSLS,BSPC, \
+        TAB ,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSPC, \
+        FN2 ,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,     ENT,  \
+        LSFT,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT, FN0, \
+             LGUI,LALT,           FN1,                RALT,RGUI
+    ),
 
-    /* Layer 1: HHKB mode (HHKB Fn)
-     * ,-----------------------------------------------------------.
-     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
-     * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
-     * |-----------------------------------------------------------|
-     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
-     * |-----------------------------------------------------------|
-     * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
-     * `-----------------------------------------------------------'
-     * |     |Gui|Alt  |         Space         |Alt  |Gui|   |     |
-     * `-----------------------------------------------------------'
-     */ 
-    [1] = 
-    KEYMAP( ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,TRNS,TRNS,   \
-           TRNS,TRNS,  UP,TRNS,TRNS,TRNS,TRNS,MPRV,MPLY,MNXT,TRNS,TRNS, TRNS, DEL,      \
-           TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,VOLD,MUTE,VOLU,TRNS,TRNS,TRNS,            \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,            \
-               TRNS,TRNS,          TRNS,               TRNS,TRNS),
+    /* 
+     * Layer 1: SpaceFN overlay
+     */
+    [1] = KEYMAP(
+        ESC,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,TRNS,TRNS, \
+        TRNS,TRNS,  UP,TRNS,TRNS,TRNS,TRNS,MPRV,MPLY,MNXT,PSCR,TRNS,TRNS, DEL, \
+        TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,VOLD,MUTE,VOLU,TRNS,TRNS,    TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,           TRNS,               TRNS,TRNS
+    ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -47,4 +29,3 @@ const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_TAP_KEY(1, KC_SPC),
     [2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
 };
-
