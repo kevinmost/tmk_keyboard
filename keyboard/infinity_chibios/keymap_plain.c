98dda49adaabf341f4a040060cf7704431001787
Kevin Most <kevin@kevinmost.com>
2016-05-18 17:38:17 -0400

    Changed "caps lock" to escape + ctrl-if-hold

--- keyboard/infinity_chibios/keymap_plain.c
+++ keyboard/infinity_chibios/keymap_plain.c
@@ -8,7 +8,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP(
         GRV ,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSLS, DEL,\
         TAB ,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSPC, \
-        CAPS,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,      ENT,  \
+         FN5,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT,      ENT,  \
         LSFT,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT,FN0, \
         LCTL,LGUI,LALT,           FN1,               LALT,RGUI, FN2,RCTL
     ),
@@ -34,6 +34,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),*/
 };
 
+// TODO: Reorganize these in some logical order... or give them names maybe that are ints? I hate C...
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_TOGGLE(1),
     [1] = ACTION_LAYER_TAP_KEY(1, KC_SPC),
@@ -42,4 +43,6 @@ const uint16_t PROGMEM fn_actions[] = {
     // Press vol down and vol up with Shift + GUI to get 1/4 step on Mac
     [3] = ACTION_MODS_KEY(MOD_LGUI | MOD_LSFT, KC_VOLD),
     [4] = ACTION_MODS_KEY(MOD_LGUI | MOD_LSFT, KC_VOLU),
+
+    [5] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
 };
